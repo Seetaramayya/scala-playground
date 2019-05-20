a3145547fa65d8ac03e8cf5e8d598fd1bcb0dacd
Swetha Tornal <47091086+swethatornal@users.noreply.github.com>
2019-05-20 20:16:21 +0200

    forgot to pass constructor argument

--- src/main/scala/com/seeta/reflection/CreateInstance.scala
+++ src/main/scala/com/seeta/reflection/CreateInstance.scala
@@ -11,9 +11,11 @@ import scala.reflect.runtime.universe
   */
 case class Person(name: String)
 object CreateInstance extends App {
+  private val loader = getClass.getClassLoader
+
   def createInstanceFromType: universe.MethodMirror = {
     //obtaining correct mirror
-    val mirror = universe.runtimeMirror(getClass.getClassLoader)
+    val mirror = universe.runtimeMirror(loader)
 
     // obtain class symbol which can be used to reflect
     val classSymbol = universe.typeOf[Person].typeSymbol.asClass
@@ -25,21 +27,22 @@ object CreateInstance extends App {
   }
 
   def createInstanceFromFullyQualifiedName(className: String): universe.MethodMirror = {
-    val mirror = universe.runtimeMirror(getClass.getClassLoader)
+    val mirror = universe.runtimeMirror(loader)
     val classSymbol = mirror.staticClass(className)
     val construtorAsMethod = classSymbol.selfType.decl(universe.termNames.CONSTRUCTOR).asMethod
     mirror.reflectClass(classSymbol).reflectConstructor(construtorAsMethod)
   }
 
-  def createInstanceFrom[T](classSymbol: universe.ClassSymbol, args: Array[Any] = Array()) = {
+  def createInstanceFrom(classSymbol: universe.ClassSymbol): universe.MethodMirror = {
     val constructor = classSymbol.selfType.decl(universe.termNames.CONSTRUCTOR).asMethod
-    universe.runtimeMirror(getClass.getClassLoader).reflectClass(classSymbol).reflectConstructor(constructor)(args).asInstanceOf[T]
+    universe.runtimeMirror(loader).reflectClass(classSymbol).reflectConstructor(constructor)
   }
 
-  def getClassSymbol(className: String): universe.ClassSymbol = universe.runtimeMirror(getClass.getClassLoader).staticClass(className)
+  def getClassSymbol(className: String): universe.ClassSymbol = {
+    universe.runtimeMirror(loader).staticClass(className)
+  }
 
   private def findAllClassInPackage(packageName: String): Set[universe.ClassSymbol] = {
-    val loader = getClass.getClassLoader
     ClassPath.from(loader).getTopLevelClasses(packageName).asScala.toList
       .map(info => universe.runtimeMirror(loader).staticClass(info.getName)).toSet
   }
@@ -49,6 +52,10 @@ object CreateInstance extends App {
   println(s"First instance of Person is $person1 and Second instance of Person is $person2")
   println(s"Reference equality is '${person1.eq(person2)}', but object equality '${person1 == person2}'")
 
-  createInstanceFrom[Person](getClassSymbol("com.seeta.Person"))
-//  findAllClassInPackage("com.seeta").map(classSymbol => createInstanceFrom[Any](classSymbol)).foreach(println)
+  println(getClassSymbol("com.seeta.reflection.Person").isClass)
+  val person = createInstanceFrom(getClassSymbol("com.seeta.reflection.Person"))("seeta").asInstanceOf[Person]
+  println(person)
+
+  findAllClassInPackage("com.seeta")
+    .filter(classSymbol => !classSymbol.isAbstract).foreach(println)
 }
